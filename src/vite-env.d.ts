fe1d688b63d672b4e2934fd160c0be34702c5a7a
Kouadio Alfred <alfred.justbelieve.5@gmail.com>
2026-07-21 15:11:30 +0000

    Remove unused ImportMetaEnv and ImportMeta interfaces from vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_AUTH_USERNAME: string
-  readonly VITE_AUTH_PASSWORD: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
