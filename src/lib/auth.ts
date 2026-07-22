027736618d9095510ccf021f4b6c355439db33db
Kouadio Alfred <alfred.justbelieve.5@gmail.com>
2026-07-22 10:36:08 +0000

    Add comment placeholder in verifierIdentifiants function

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,3 +3,6 @@ import { ACCES_AUTORISE } from "./access"
 export function verifierIdentifiants(nomUtilisateur: string, motDePasse: string): boolean {
   return nomUtilisateur === ACCES_AUTORISE.nomUtilisateur && motDePasse === ACCES_AUTORISE.motDePasse
 }
+
+
+/**C'est une fonction qui */
